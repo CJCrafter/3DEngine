29bc8a5bfa51461340a7e6d7fc054c25bac8eae0
CJCrafter <collinjbarber@gmail.com>
2021-11-19 10:45:31 -0800

    Added assimp

--- 3DEngine/Application.cpp
+++ 3DEngine/Application.cpp
@@ -9,16 +9,25 @@
 #include "GDIPlusManager.h"
 #include "imgui_impl_dx11.h"
 #include "imgui_impl_win32.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 GDIPlusManager gdi;
 
 Application::Application()
 	:
-	window(800, 800, "Testing testing 123"),
+	window(1000, 1000, "3D CUBES"),
 	timer(0.005f),
-	camera(8.0f, -45.0f * 3.1415f / 180.0f, 45.0f * 3.1415f / 180.0f, {0.0f, 0.0f, 0.0f}),
+	camera(8.0f, -3.1415f / 4.0f, 3.1415f / 4.0f, {0.0f, 0.0f, 0.0f}),
 	light(window.GetGraphics())
 {
+	Assimp::Importer importer;
+	auto model = importer.ReadFile("mustang.obj",
+		aiProcess_Triangulate | 
+		aiProcess_JoinIdenticalVertices
+		);
+
 	std::mt19937 rand(std::random_device{}());
 	std::uniform_real_distribution a(0.0f, 3.1415f * 2.0f);
 	std::uniform_real_distribution b(0.0f, 3.1415f * 0.25f);
@@ -27,17 +36,17 @@ Application::Application()
 	std::uniform_real_distribution e(0.25f, 2.0f);
 	std::uniform_real_distribution f(0.0f, 1.0f);
 
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 100; i++)
 	{
 		shapes.push_back(std::make_unique<MirahCube>(window.GetGraphics()));
 		auto& temp = shapes.back();
-		//temp->position = { a(rand), a(rand), a(rand) };
-		//temp->velocity = { c(rand), c(rand), c(rand) * 2 };
-		//temp->rotation = { b(rand), b(rand), b(rand) };
+		temp->position = { a(rand), a(rand), a(rand) };
+		temp->velocity = { c(rand), c(rand), c(rand) };
+		temp->rotation = { b(rand), b(rand), b(rand) };
 		temp->angle = { PI / 40.0f, PI / 40.0f, 0.0f };
 		temp->scale    = Vec3f{ 1.0f, 1.0f, 1.0f };
 	}
-	window.GetGraphics().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 1.0f, 0.5f, 40.0f));
+	window.GetGraphics().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 1.0f, 0.5f, 80.0f));
 }
 
 int Application::Start()
@@ -126,14 +135,16 @@ void Application::Update(const float delta)
 		Vec3f& rotation = shape->rotation;
 		constexpr float rate = PI / 4;
 
+		/*
 		if (window.key.KeyIsPressed('W')) rotation.x = rate;
 		else if (window.key.KeyIsPressed('S')) rotation.x = -rate;
 		if (window.key.KeyIsPressed('A')) rotation.z = rate;
 		else if (window.key.KeyIsPressed('D')) rotation.z = -rate;
 		if (window.key.KeyIsPressed('Q')) rotation.y = rate;
 		else if (window.key.KeyIsPressed('E')) rotation.y = -rate;
+		*/
 
 		shape->Tick(delta);
-		rotation.Clear();
+		//rotation.Clear();
 	}
 }
